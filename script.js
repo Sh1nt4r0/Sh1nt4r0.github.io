8e1f146c8182f2d2cb16f4ffb33e2e4560f21a50
Sh1nt4r0 <samslofton@gmail.com>
2016-10-03 21:27:40 +0000

    create Cringe website

--- script.js
+++
@@ -1,8 +0,0 @@
- var displayAnswers = function (){
-                var name = document.getElementById("idTxtName").value;
-                var club = document.getElementById("idTxtClub").value;
-                var Sport = document.getElementById("idTxtFavSport").value;
-                var output="";
-                output += "Your name is " + name + "<br>" + "You are in " + club + "<br>" + "Your favorite sport is " + Sport;
-                document.getElementById("divtext").innerHTML = output;
-            };
